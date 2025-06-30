a7bd4c3d5d74c8fd5ff40d0a022f626979627c24
igorrossi20 <igor.rossi10@gmail.com>
2025-06-29 23:15:19 -0300

    1.0

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 // Renomeie este arquivo para firebaseConfig.js e preencha com suas credenciais do Firebase
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBeOBlFS3jbh0uM3t0T71LQnv14J9yRLWI",
+  authDomain: "pescador-79e00.firebaseapp.com",
+  projectId: "pescador-79e00",
+  storageBucket: "pescador-79e00.firebasestorage.app",
+  messagingSenderId: "238257791759",
+  appId: "1:238257791759:web:0469223871634082f7947a",
 };
 export const appId = 'ranking-da-vara'; 
